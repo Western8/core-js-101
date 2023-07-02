e42caf2f2a2e8c1ca806ab2f16162fb35f1dc4c7
Western8 <westernchrome@gmail.com>
2023-07-02 13:11:54 +0300

    feat: implement number func

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -137,9 +137,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
-  // return value.toString().at(-1);
+function getLastDigit(value) {
+  return value.toString().at(-1);
 }
 
 
